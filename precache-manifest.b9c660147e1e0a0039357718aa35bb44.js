2bd81b1bdfaf2265b566ea017d69a431d361585e
Levitate <parmonmd@gmail.com>
2021-03-22 18:42:42 +0300

    Updates

---
+++ precache-manifest.b9c660147e1e0a0039357718aa35bb44.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d870529a5c6677dcfda2f29dbe3842d",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "389507c59eecf868ca01",
+    "url": "/social/static/css/3.69dea3a1.chunk.css"
+  },
+  {
+    "revision": "0b5d29f8b63a5cb41ac8",
+    "url": "/social/static/css/4.63970abc.chunk.css"
+  },
+  {
+    "revision": "28549766338bc709e68a",
+    "url": "/social/static/css/5.5bdae286.chunk.css"
+  },
+  {
+    "revision": "86060322a5a1e45d76d4",
+    "url": "/social/static/css/main.55104cac.chunk.css"
+  },
+  {
+    "revision": "fde309e81cb20d295db6",
+    "url": "/social/static/js/2.88b5cfaa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/social/static/js/2.88b5cfaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389507c59eecf868ca01",
+    "url": "/social/static/js/3.cb9937ea.chunk.js"
+  },
+  {
+    "revision": "0b5d29f8b63a5cb41ac8",
+    "url": "/social/static/js/4.41870ea5.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/social/static/js/4.41870ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28549766338bc709e68a",
+    "url": "/social/static/js/5.c589c55d.chunk.js"
+  },
+  {
+    "revision": "86060322a5a1e45d76d4",
+    "url": "/social/static/js/main.96957c84.chunk.js"
+  },
+  {
+    "revision": "ebbd973757ee860a5334",
+    "url": "/social/static/js/runtime-main.68eac6eb.js"
+  },
+  {
+    "revision": "28d599078abebef92ac495c97bc19baf",
+    "url": "/social/static/media/preloader.28d59907.gif"
+  },
+  {
+    "revision": "2df8c64c9b9c1b042363cec238c037a2",
+    "url": "/social/static/media/user.2df8c64c.png"
+  }
+]);
